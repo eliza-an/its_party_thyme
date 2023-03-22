6162886ed2d97f8faf27c198bc9bf1665c1af47c
Laurence Burt <laurencemb@hotmail.co.uk>
2023-03-22 15:14:43 +0000

    button component index and style files

---
+++ my-react-app/src/Components/signupButton/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function SignUpBtn(props) {
+    return (
+        <button
+            onClick={props.onClick}
+            className={`signUpBtn ${props.type}`}
+            data-value={props.type}
+            >
+                {props.type}
+        </button>
+    )
+}
