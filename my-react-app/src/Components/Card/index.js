6d220d2d2f04681813eeae455ddd16dcdd9ef9a2
Laurence Burt <laurencemb@hotmail.co.uk>
2023-03-22 16:27:43 +0000

    added card css

--- my-react-app/src/Components/Card/index.js
+++ my-react-app/src/Components/Card/index.js
@@ -3,13 +3,13 @@ import React from 'react;
 export default function Plants({plant}) {
     return (
         <div className='card'>
-            <img src={plant.default_image.small_url} alt={plant.common_name} /> /*  I think this is correct route through the data set for the image*/
+            <img className='cardImage' src={plant.default_image.small_url} alt={plant.common_name} /> {/* /*  I think this is correct route through the data set for the image*/ }
         
          <div className='text-container'>
-            <h4>{plant.common_name}</h4>
+            <h4 className='plantTitle'>{plant.common_name}</h4>
             <p className="wateringStatus"> {plant.common_name} needs {plant.watering} </p>
-            <p classname="moreDetails"> Click here for more care information about {plant.common_name} </p>
-            </div>
+            <p classname="moreDetails"> Click here for more care information about {plant.common_name} </p> {/* add modal opening functionality later  */}
+             </div>
         
      </div>
 
