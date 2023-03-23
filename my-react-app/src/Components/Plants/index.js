5a72f2a1dfc2ba9e993ae4fef05a3f6fc0c0cfd1
Fi Dunnington <fi.dunnington@gmail.com>
2023-03-23 11:05:37 +0100

    query for Plant List data. Have not added plant detail data yet

--- my-react-app/src/Components/Plants/index.js
+++ my-react-app/src/Components/Plants/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 
-export default function Characters() {
-    const fetchCharacters = async () => {
-        const response = await fetch("https://rickandmortyapi.com/api/character");
-        return response.json();
+
+export default function Plants() {
+        
+    const fetchPlants = async () => {
+        //To fetch fresh data on every fetch request using cache: no-store
+        const response = await fetch("https://perenual.com/api/species-list?page=1&key=sk-htIX6419d1b8cfc9f275", { cache: 'no-store' });
+        return response.json(); 
     };
 
-    const {data, status} = useQuery("characters", fetchCharacters([]))
+    const {data, status} = useQuery("plants", fetchPlants);
     
-    console.log(status);
-
-    if (status === "loading") {
-        return <div>Loading...</div>
+    if(status === "loading") {
+        <div>Loading...</div>
     };
 
-    if (status === "error") {
-        return <div>Error</div>
+    if(status === "error") {
+    <div>Error</div>
     };
 
     return (
-    <div>
-        {data.results.map((character) => (
-            <div>{character.name}</div>
-        ))}
-    </div>
-  )
-}
+        <div>
 
+        </div>
+    );
+
+};
