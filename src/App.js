61cedb825f6af907c22e672411bbf3fe8563f1af
Laurence Burt <laurencemb@hotmail.co.uk>
2023-03-23 16:39:10 +0000

    added Search to app.js and frgaments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React, {useState, Fragment} from React;
 import Firstpage from "./Components/first-page/firstpage";
+import Search from './Components/Search/index'
+
+function App ({plants}) {
+  const [searchValue, setSearchValue] = useState('')
 
 const App = () => {
   return(
- <Firstpage/>)
+    <Fragment>
+ <Firstpage/>
+ <Search searchValue={searchValue} setSearchValue={setSearchValue}/>
+ </Fragment>
+ )
 };
+}
 
 export default App;
