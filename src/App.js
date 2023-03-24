8cb403ee41c52763cd2e297e7daaca81d0259272
Fi Dunnington <fi.dunnington@gmail.com>
2023-03-24 16:39:14 +0100

    added second-page component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
 import React, {/*useState,*/ Fragment} from 'react';
-// import Column from "./Components/Column";
 import Firstpage from "./Components/first-page/firstpage";
-// import Navbar from "./Components/Nav";
-// import Row from "./Components/Row";
+import Plants from "./Components/Plants";
+
+import {QueryClientProvider, QueryClient} from "react-query";
+
 // import Search from './Components/Search'
+
+const queryClient = new QueryClient()
 
 
 const App = () => {
   return(
    <Fragment>
-    <Firstpage/>
-    {/* <Search searchValue={searchValue} setSearchValue={setSearchValue}/> */}
+    <QueryClientProvider client={queryClient}>
+      {/* < Plants /> */}
+      <Firstpage/>
+      {/* <Search searchValue={searchValue} setSearchValue={setSearchValue}/> */}
+    </QueryClientProvider>
    </Fragment>
  )
 };
---
+++ src/Components/second-page/firstpage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Column from "../Column";
+import Nav from "../Nav";
+
+import "style.css"
+
+
+const Secondpage = () =>{
+
+    return(
+        <div>
+            <Nav/>
+            <Column />
+            <Column />
+        </div>
+    )
+
+}
+
+export default Secondpage
+
