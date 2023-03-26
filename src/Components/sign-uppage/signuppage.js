2ffaad6c1260bcf5a35434699ddea2776c49cd90
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-03-26 15:53:25 +0100

    everything is linked ok

--- src/Components/sign-uppage/signuppage.js
+++ src/Components/sign-uppage/signuppage.js
@@ -4,6 +4,7 @@ import Login from "../first-page/login/login";
 //import Column from "../Column";
 
 import SignUpBtn from "../signupButton/index";
+import { Link } from "react-router-dom";
 
 
 const SignUp= () =>{
@@ -13,9 +14,9 @@ return(
 <div className="First">
     <img className="flowerImg" alt="flower" src="./img/flower.jpg"></img>< div className="plantly"><h1>PLANTLY</h1></div> 
 
-    <div className="plantly"><h3>Please enter a you name and create a password</h3></div>
+    <div className="plantly"><h3>Please enter your name and create a password</h3></div>
     <div className="plantly"><Login /></div>
-    <div className="plantly"><SignUpBtn className="signUpBtn" text= 'Signup and Login'></SignUpBtn> </div>  
+    <div className="plantly"><Link to= "/"><SignUpBtn className="signUpBtn" text= ' <== Already have an account?'></SignUpBtn></Link> </div>  
 </div>
 
 )
