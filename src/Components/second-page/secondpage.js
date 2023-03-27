9b6d2a298f6a911de105edf39155aaa0893e859e
Laurence Burt <laurencemb@hotmail.co.uk>
2023-03-27 17:24:14 +0100

    more searchbar work still not functioning

--- src/Components/second-page/secondpage.js
+++ src/Components/second-page/secondpage.js
@@ -11,7 +11,7 @@ function Secondpage () {
     const [searchValue, setSearchValue] = useState('')
 console.log(searchValue)
 
-const fetchPlants = async () => {
+const fetchPlants = async (searchValue) => {
     //To fetch fresh data on every fetch request using cache: no-store
     const response = await fetch(`https://perenual.com/api/species-list?page=1&key=sk-htIX6419d1b8cfc9f275&q=${searchValue}`, { cache: 'no-store' });
     return response.json(); 
@@ -19,14 +19,14 @@ const fetchPlants = async () => {
 
 
 
-/* const {data, status} = useQuery("plants", () => fetchPlants(searchValue), { enabled: !!searchValue });
+const {data, status} = useQuery("plants", () => fetchPlants(searchValue), { enabled: !!searchValue });
+const plantList = data || [];
+console.log(plantList);
+
+
+/* const {data, status} = useQuery("plants", fetchPlants);
 const plantList = data;
-console.log(plantList); */
-
-
-const {data, status} = useQuery("plants", fetchPlants);
-const plantList = {data};
-console.log(plantList); 
+console.log(plantList);  */
 
 
 
@@ -35,12 +35,20 @@ console.log(plantList);
 
     return(
         
-            <QueryClientProvider client={queryClient} contextSharing={true}>
+            <QueryClientProvider client={queryClient} >
             <Nav/>
             <Column />
             <Column />
             <Search  searchValue={searchValue} setSearchValue={setSearchValue} />
-         
+            {status === "loading" && <div>Loading data...</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <ul>
+          {plantList.map((plant) => (
+            <li key={plant.id}>{plant.name}</li>
+          ))}
+        </ul>
+      )}
     
         </QueryClientProvider>
     )
@@ -50,3 +58,25 @@ console.log(plantList);
 export default Secondpage;
 
 
+{/* <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+
+{useQueries([
+  { queryKey: "plants", queryFn: fetchPlants }
+]).map((query, index) => {
+  const { data, status } = query;
+  const plantList = data;
+
+  return (
+    <React.Fragment key={index}>
+      {status === "loading" && <div>Loading data...</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <ul>
+          {plantList.map((plant) => (
+            <li key={plant.id}>{plant.name}</li>
+          ))}
+        </ul>
+      )}
+    </React.Fragment>
+  )
+})} */}
