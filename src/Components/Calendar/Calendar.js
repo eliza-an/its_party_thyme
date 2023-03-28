b72a70cb58b73427776c8066d2c36cf8377311e6
Moorgal <megaboki@gmail.com>
2023-03-29 00:18:19 +0100

    Calendar select option

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -6,15 +6,14 @@ import { useEffect } from 'react';
 import './Calendar.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-
 import Navbar from '../Nav';
-// import Row from '../Row';
 import Column from '../Column';
 
 // npm i react-datepicker
 // npm i react-big-calendar
 
 let username = 'partythyme';
+
 const localizer = momentLocalizer(moment);
 function CreateCalendar() {
   let localKeys = Object.keys(localStorage);
@@ -23,6 +22,9 @@ function CreateCalendar() {
   localKeys.includes(username) ? (house = JSON.parse(localStorage.getItem(username))) : localStorage.setItem(username, '[]');
   //  check if username exist                if yes  then house = username value
   localKeys.includes(username) ? (house = JSON.parse(localStorage.getItem(username))) : console.log('ej ej something is not okey Pete');
+
+  let plantsArray = JSON.parse(localStorage.getItem('myplants'));
+
   // create one plant
   const [plant, setPlant] = useState({ title: '', start: '', end: '' });
   // create group of plants (when you with click button createArray run) and adds the data to "house" variable
@@ -31,16 +33,11 @@ function CreateCalendar() {
     // all the existing plants plus add the new plant
     setAllPlant([...plants, plant]);
   };
-  // upload data to localstorage
-  // const createLocalStorage = () => {
-  //   localStorage.setItem(username, JSON.stringify(plants));
-  // };
 
   useEffect(() => {
     localStorage.setItem(username, JSON.stringify(plants));
   }, [plants]);
 
-  console.log(plants);
   return (
     <div className="App">
       <Navbar />
@@ -49,11 +46,18 @@ function CreateCalendar() {
           <h2>add new plant</h2>
           <div>
             {/* update with data the existing empty plant variable */}
-            <input type="text" placeholder="Plant" value={plant.title} onChange={(title) => setPlant({ ...plant, title: title.target.value })} />
+
+            <select value={plant.title} onChange={(title) => setPlant({ ...plant, title: title.target.value })}>
+              <option>--select option</option>
+              {plantsArray.map((plant, index) => (
+                <option key={index} value={plant.common_name}>
+                  {plant.common_name}
+                </option>
+              ))}
+            </select>
             <DatePicker placeholderText="Date" selected={plant.start} onChange={(start) => setPlant({ ...plant, start: start })} />
             <DatePicker placeholderText="Date" selected={plant.end} onChange={(end) => setPlant({ ...plant, end: end })} />
             <button onClick={createArray}>Add Plant</button>
-            {/* <button onClick={createLocalStorage}>Save Data</button> */}
           </div>
         </Column>
         <Column>
